7ecada76c714cdbcad243f0bbab00483fdb6d918
Joallyson-Pereira <joallysonpere@gmail.com>
2022-07-17 23:01:31 -0300

    novo

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //Estou aprendendo desenvolvimento web!!
+
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+
+// Classe Show
+function mostrar() {  
+    document.querySelector('.nav').classList.add('show')
+    
+}
+
+function remover() {
+  document.querySelector('.nav').classList.remove('show')
+}
+
+
+
+
